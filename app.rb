8e27dc4eca0122c1f2da64930a2d34368afd6513
moby-codes <mubashar.jaffri@gmail.com>
2021-08-11 16:16:15 +0100

    redone step 1 for setup, added class Battle and Sinatra::Reloader set up first test using Capybara and made test fail correctly and then pass accordingly

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/reloader'
 
-get '/battle' do
-  "Hello Battle!"
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+
+get '/' do
+  "Testing infrastructure working!"
+end
+
+# start the server if ruby file executed directly
+run! if app_file ==$0
 end
---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,8 @@
+feature 'Testing Infrastructure' do
+
+  scenario 'Can run app and checks for content' do
+    visit('/') #visiting the root page
+    expect(page).to have_content 'Testing infrastructure working!'
+
+  end
+end
